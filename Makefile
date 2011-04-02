90039a08e5dc451d8f9d8075926edfb662dbdedd
Robert BuRnEr Schadek <rburners@gmail.com>
2011-04-02 17:59:14 +0200

    a nice multimap start

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 all: crude
-FILES = dex/main.d dex/bitmap.d dex/fsm.d dex/list.d dex/util.d dex/set.d
+FILES = dex/main.d dex/bitmap.d dex/fsm.d dex/list.d dex/util.d dex/set.d \
+dex/multimap.d
 
 crude:
 	#dmd dex/*.d -offsm -unittest libphobos2.a libhurt.a -I../libhurt/ -gc
-	#dmd $(FILES) -offsm -unittest libphobos2.a libhurt.a -I../libhurt/ -gc
-	dmd $(FILES) -offsm -unittest libhurt.a -I../libhurt/ -gc
+	dmd $(FILES) -offsm -unittest libphobos2.a libhurt.a -I../libhurt/ -gc
+	#dmd $(FILES) -offsm -unittest libhurt.a -I../libhurt/ -gc
---
+++ dex/multimap.d
@@ -0,0 +1,107 @@
+module dex.multimap;
+
+class MultiMap(T,S) {
+	private class Value(S) {
+		uint idx;
+		S[] multi;
+
+		this(S value) {
+			this.multi = new S[16];
+			this.multi[this.idx++] = value;
+		}
+
+		public void insert(S value) {
+			if(this.idx == multi.length) {
+				this.multi.length *= 2u;
+			}
+			this.multi[this.idx++] = value;
+		}
+
+		public S[] remove() {
+			return this.multi[0..this.idx].dup;
+		}
+
+		public S[] remove(uint rIdx) {
+			if(rIdx >= this.idx) {
+				assert(0, "not allowed to remove out of index");
+			}
+			uint upIdx = rIdx + 1u;
+			uint lowIdx = rIdx;
+			while(lowIdx < this.idx - 1u) {
+				this.multi[lowIdx] = this.multi[upIdx];
+				upIdx++;
+				lowIdx++;
+			}
+			if(this.idx == 1u) {
+				return null;
+			} else {
+				this.idx--;
+				return this.multi[0..this.idx];
+			}
+		}
+
+		public uint getSize() {
+			return this.idx;
+		}
+
+		public S[] values() {
+			return this.multi[0..this.idx];
+		}
+	}
+
+	Value!(S)[T] multi;
+
+	MultiMap!(T,S) insert(T key, S value) {
+		if(key in this.multi) {
+			Value!(S) tmp = this.multi[key];
+			tmp.insert(value);
+		} else {
+			this.multi[key] = new Value!(S)(value);
+		}
+		return this;
+	}
+
+	S[] remove(T key, uint idx) {
+		if(key in this.multi) {	
+			S[] tmp = this.multi[key].remove(idx);	
+			if(tmp is null) {
+				return null;
+			} else {
+				return tmp;
+			}
+		} else {
+			return null;
+		}
+	}
+
+	S[] remove(T key) {
+		if(key in this.multi) {	
+			S[] tmp = this.multi[key].remove();	
+			this.multi.remove(key);
+			return tmp;
+		} else {
+			return null;
+		}
+	}
+
+	S[] find(T key) {
+		if(key in this.multi) {	
+			S[] tmp = this.multi[key].remove();	
+			return tmp;
+		} else {
+			return null;
+		}
+	}
+}
+
+unittest {
+	MultiMap!(char,int) mm1 = new MultiMap!(char,int)();
+	mm1.insert('t', 12);
+	mm1.insert('t', 22);
+	mm1.insert('t', 32);
+	mm1.insert('t', 42);
+	assert(mm1.find('t') == [12,22,32,42]);
+	mm1.remove('t', 0u);
+	assert(mm1.find('t') == [22,32,42]);
+
+}
