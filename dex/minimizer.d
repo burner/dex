1d02c46ea67299aa5700579835e9d1f979e105bc
Robert BuRnEr Schadek <rburners@gmail.com>
2011-04-26 21:39:57 +0200

    save

--- dex/minimizer.d
+++ dex/minimizer.d
@@ -7,6 +7,10 @@ import hurt.container.dlst;
 
 import std.stdio;
 
+public State merge(ref Vector!(State) toMerge) {
+
+}
+
 public DLinkedList!(State) minimize(DLinkedList!(State) oldStates) {
 	bool changes = true;
 	Vector!(State) sameStates = new Vector!(State)(32);
